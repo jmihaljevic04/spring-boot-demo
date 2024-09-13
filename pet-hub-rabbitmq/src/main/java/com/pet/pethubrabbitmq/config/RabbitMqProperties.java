5fc4ceed064cb6c1304b09909edce911bf403b6d
jmihaljevic04 <josipmiha4@gmail.com>
2024-09-13 18:04:02 +0200

    Define property source to security and RabbitMQ module with custom property files

--- pet-hub-rabbitmq/src/main/java/com/pet/pethubrabbitmq/config/RabbitMqProperties.java
+++ pet-hub-rabbitmq/src/main/java/com/pet/pethubrabbitmq/config/RabbitMqProperties.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "pet.rabbitmq")
 @EnableConfigurationProperties(RabbitMqProperties.class)
+@PropertySource("classpath:rabbitmq.properties")
 public class RabbitMqProperties {
 
     private boolean enabled;
--- pet-hub-security/src/main/java/com/pet/pethubsecurity/config/JwtProperties.java
+++ pet-hub-security/src/main/java/com/pet/pethubsecurity/config/JwtProperties.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties(prefix = "pet.jwt")
 @EnableConfigurationProperties(JwtProperties.class)
+@PropertySource("classpath:security.properties")
 public class JwtProperties {
 
     private String secretKey;
