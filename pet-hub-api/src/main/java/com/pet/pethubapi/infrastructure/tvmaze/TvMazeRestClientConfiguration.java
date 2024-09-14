77e20742c734ee9d6f7c6924b937bbc2c04f062b
jmihaljevic04 <josipmiha4@gmail.com>
2024-09-14 18:53:32 +0200

    Implement external HTTP calls logging

--- pet-hub-api/src/main/java/com/pet/pethubapi/infrastructure/tvmaze/TvMazeRestClientConfiguration.java
+++ pet-hub-api/src/main/java/com/pet/pethubapi/infrastructure/tvmaze/TvMazeRestClientConfiguration.java
@@ -1,6 +1,8 @@
 package com.pet.pethubapi.infrastructure.tvmaze;
 
 import com.pet.pethubapi.domain.ApiApplicationProperties;
+import com.pet.pethubapi.infrastructure.logging.RestClientRequestLogger;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +21,9 @@ class TvMazeRestClientConfiguration {
     private static final RestClient.Builder TV_MAZE_REST_CLIEN_BUILDER = RestClient.builder()
         .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
         .defaultHeader(HttpHeaders.CACHE_CONTROL, CacheControl.maxAge(Duration.ZERO).getHeaderValue())
-        .defaultStatusHandler(new TvMazeRestClientErrorHandler());
+        .defaultStatusHandler(new TvMazeRestClientErrorHandler())
+        .requestInterceptor(new RestClientRequestLogger())
+        .observationRegistry(ObservationRegistry.create());  // for Micrometer observability: https://docs.spring.io/spring-framework/reference/integration/observability.html#observability.http-client.restclient
 
     private final ApiApplicationProperties applicationProperties;
     @Value("${spring.application.name}")
