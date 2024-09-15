79fe3c842acd1b56a534eeb86f9ce5b4db88e5c8
jmihaljevic04 <josipmiha4@gmail.com>
2024-09-15 12:41:07 +0200

    Implement HATEOAS on fuzzy search to fetch show by provided ID

--- pet-hub-api/src/main/java/com/pet/pethubapi/domain/tvshow/TvShowDetailsRepository.java
+++ pet-hub-api/src/main/java/com/pet/pethubapi/domain/tvshow/TvShowDetailsRepository.java
@@ -1,11 +1,14 @@
 package com.pet.pethubapi.domain.tvshow;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TvShowDetailsRepository {
 
     List<TvShowDTO> getAllShows(int pageNumber);
 
     List<TvShowSearchResponse> getShowDetailsByName(String name);
+
+    Optional<TvShowDTO> getShowDetailsById(Long id);
 
 }
