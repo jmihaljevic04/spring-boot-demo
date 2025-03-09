5d3a705b57c7f980c6c2cefa5f164a74c097caea
jmihaljevic04 <josipmiha4@gmail.com>
2025-03-09 16:05:12 +0100

    Import country list
    List of countries with their ISO 3166-1 codes.

---
+++ pet-hub-batch/src/main/java/com/pet/pethubbatch/domain/country/Country.java
@@ -0,0 +1,26 @@
+package com.pet.pethubbatch.domain.country;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "country")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "alpha_2_code")
+    private String isoCode;
+
+}
