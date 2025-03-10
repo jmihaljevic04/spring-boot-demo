56c6c7645995aafa89bf71c4162dd634818c7d94
jmihaljevic04 <josipmiha4@gmail.com>
2025-03-10 20:28:37 +0100

    Correct security configuration for JsonPlaceholder feature

--- pet-hub-security/src/main/java/com/pet/pethubsecurity/config/web/SecurityConfiguration.java
+++ pet-hub-security/src/main/java/com/pet/pethubsecurity/config/web/SecurityConfiguration.java
@@ -37,7 +37,7 @@ class SecurityConfiguration {
             .authorizeHttpRequests(request -> request
                 .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/actuator/mock/**").permitAll()
+                .requestMatchers("/mock/**").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.GET, "/auth/refresh-token").permitAll()
