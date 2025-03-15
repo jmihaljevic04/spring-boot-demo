2a9b2ad289b15ba877f76afc735bb06580e29b6c
jmihaljevic04 <josipmiha4@gmail.com>
2025-03-15 13:17:47 +0100

    Implement weather station entity and initial scaffolding
    Remove redundant @Repository annotations from beans extending JpaRepository.

---
+++ pet-hub-batch/src/main/java/com/pet/pethubbatch/domain/weatherstation/meteostat/MeteostatWeatherStationDTO.java
@@ -0,0 +1,52 @@
+package com.pet.pethubbatch.domain.weatherstation.meteostat;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MeteostatWeatherStationDTO {
+
+    private String id;
+    private String name;
+    private String country;
+    private String timezone;
+    private Double latitude;
+    private Double longitude;
+    private Double elevation;
+
+    @JsonSetter("name")
+    void deserializeName(NameWrapper val) {
+        this.name = val != null ? val.en : null;
+    }
+
+    @JsonSetter("location")
+    void deserializeLatitude(LocationWrapper val) {
+        this.latitude = val != null ? val.latitude : null;
+        this.longitude = val != null ? val.longitude : null;
+        this.elevation = val != null ? val.elevation : null;
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @SuppressWarnings("unused")
+    private static class NameWrapper {
+
+        private String en;
+
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @SuppressWarnings("unused")
+    private static class LocationWrapper {
+
+        private Double latitude;
+        private Double longitude;
+        private Double elevation;
+
+    }
+
+}
