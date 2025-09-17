a595943c6075cb6968109af73c2edcd1c43fa7d7
jmihaljevic04 <josipmiha4@gmail.com>
2025-09-17 21:36:42 +0200

    Rework setters on BaseEntity classes

--- pet-hub-batch/src/main/java/com/pet/pethubbatch/domain/BaseEntity.java
+++ pet-hub-batch/src/main/java/com/pet/pethubbatch/domain/BaseEntity.java
@@ -3,7 +3,6 @@ package com.pet.pethubbatch.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Version;
-import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,28 +15,26 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @ToString
 @Getter
-@Setter
 @MappedSuperclass
 public abstract class BaseEntity {
 
-    @Setter(AccessLevel.NONE)
     @CreationTimestamp
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Setter
     @Column(name = "created_by")
     private String createdBy;
 
-    @Setter(AccessLevel.NONE)
     @UpdateTimestamp
     @Column(name = "last_updated_at")
     private LocalDateTime lastUpdatedAt;
 
+    @Setter
     @Column(name = "last_updated_by")
     private String lastUpdatedBy;
 
     @Version
-    @Setter(AccessLevel.NONE)
     @Column
     private Integer version;
 
